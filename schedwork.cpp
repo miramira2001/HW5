c8d0f218eebe5bbacd567a9023fcfd213f878bd8
Miracle Ini-Abasi <iniabasi@usc.edu>
2023-04-07 03:35:25 +0000

    “HW5”

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,4 +1,3 @@
-#ifndef RECCHECK
 #include <set>
 #include <iostream>
 #include <fstream>
@@ -7,9 +6,6 @@
 #include <map>
 #include <algorithm>
 // add or remove necessary headers as you please
-
-#endif
-
 #include "schedwork.h"
 
 using namespace std;
@@ -21,10 +17,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
-
+bool makeSched(size_t hor, size_t ver, size_t count, size_t need, vector<size_t> move, 
+	const AvailabilityMatrix& avail, DailySchedule& sched);
 // Add your implementation of schedule() and other helper functions here
-
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -36,10 +31,41 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
-
-
-
-
+		//Add code below
+		//make the vector 
+vector<size_t> move(avail[0].size()); std::fill(move.begin(), move.end(), 0);
+    // make sched
+std::vector<Worker_T> vec(dailyNeed);
+	DailySchedule curr(avail.size(), vec);
+bool result = makeSched(size_t{}, size_t{}, maxShifts, dailyNeed, move, avail, curr);
+	sched = curr;
+	return result;
 }
-
+bool makeSched(size_t hor, size_t ver, size_t count, size_t need, vector<size_t> move, 
+	const AvailabilityMatrix& avail, DailySchedule& sched){
+	switch (hor == avail.size()) {
+    case true:
+        return true;
+    default:
+    bool found = false;
+        for (size_t j = 0; avail[0].size() > j ; j++) {
+    if (avail[hor][j] == 1 && count != move[j]) {
+        sched[hor][ver] = j;
+        move[j] = move[j] + 1;
+        //finish sched
+        const size_t vertical_count = sched[0].size();
+        size_t clength = (ver + 1) % vertical_count;
+        size_t hlength = hor + (ver + 1) / vertical_count;
+        found = makeSched(hlength, clength, count, need, move, avail, sched);
+        if (found) {
+            return true;
+        } else {
+            //delete
+            move[j]--;
+            sched[hor][ver] = 0;
+        }
+    }
+}
+return found;
+}
+}
--- wordle.cpp
+++ wordle.cpp
@@ -1,19 +1,14 @@
-#ifndef RECCHECK
 // For debugging
 #include <iostream>
 // For std::remove
 #include <algorithm> 
 #include <map>
 #include <set>
-#endif
-
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
-
-
 // Add prototypes of helper functions here
-
+void Answer(const std::set<std::string>& dict, std::string floating, size_t add, std::set<std::string>& right, std::string curr);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +17,61 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::set<std::string> wordle;
+    Answer(dict, floating, 0, wordle, in);
+    return wordle;
 }
-
 // Define any helper functions here
+void Answer(const std::set<std::string>& dict, std::string floating,
+            size_t add, std::set<std::string>& right, std::string curr) {
+    int num = 0;
+
+    switch(add == curr.size() && floating.empty()) {
+        case true: {
+            if (dict.find(curr) != dict.end()) {
+                right.insert(curr);
+            }
+            break;
+        }
+        case false: {
+            if (curr[add] != '-') {
+                Answer(dict, floating, add + 1, right, curr);
+                break;
+            }
+            else {
+                for (size_t j = 0; j < curr.size(); j++) {
+                    if (curr[j] == '-') {
+                        num = num + 1;
+                    }
+                }
+                string currVar = floating;
+                if ((int)currVar.size() == num) {
+                    // go through all the floats 
+                    for (size_t j = 0; j < currVar.size(); j++) {
+                        curr[add] = currVar[j];
+                        string copy = currVar;
+                        copy.replace(j, 1, "");
+                        Answer(dict, copy, add + 1, right, curr);
+                    }
+                }
+                else if (num > (int)currVar.size()) {
+                    // go through a to z 
+                    for (int track = 97; track <= 122; track++) {
+    switch (currVar.find(track)) {
+        case string::npos:
+            curr[add] = track;
+            Answer(dict, currVar, add + 1, right, curr);
+            break;
+        default:
+            curr[add] = track;
+            string copy = currVar;
+            copy.replace(copy.find(track), 1, "");
+            Answer(dict, copy, add + 1, right, curr);
+            break;
+    }
+}
+            }
+        }
+    }
+}
+            }
